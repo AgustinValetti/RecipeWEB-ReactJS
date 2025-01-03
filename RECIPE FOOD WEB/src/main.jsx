4f44d5059c46f265159087bdf308b94e51f49e3c
Agustin <agustinvaletti@gmail.com>
2025-01-03 16:12:36 -0300

    fix

--- RECIPE FOOD WEB/src/main.jsx
+++ RECIPE FOOD WEB/src/main.jsx
@@ -11,14 +11,14 @@ import './Components/Header/Nav.css'
 import './Components/Hero/Hero.css'
 
 import './Components/Footer/Footer.css'
-import './Components/Api/Api.css'
+import './Components/Recipe/Api.css'
 import './Components/Contact/Contact.css'
 
 import Nav from './Components/Header/Nav.jsx'
 import Footer from './Components/Footer/Footer.jsx'
 import Hero from './Components/Hero/Hero.jsx'
 import Contact from './Components/Contact/Contact.jsx'
-import Api from './Components/Api/Api.jsx'
+import Api from './Components/Recipe/Api.jsx'
 
 
 createRoot(document.getElementById('root')).render(
